e944284428ee39cfbeb64512e5a846b4763110e7
Matheus Kamer <matheus_kamer@hotmail.com>
2025-03-03 17:16:02 -0400

    feat: update index component with header and logo; add styles for layout

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,11 +1,22 @@
-import { Text, View } from "react-native";
+import { Image, View, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
 import { styles } from "./styles";
+import { colors } from "@/styles/colors";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>My App</Text>
-      <Text style={styles.title}>React Native!</Text>
+      <View style={styles.header}>
+        <Image
+          source={require("@/assets/logo.png")}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+        <TouchableOpacity>
+          <MaterialIcons name="add" size={32} color={colors.green[300]} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
