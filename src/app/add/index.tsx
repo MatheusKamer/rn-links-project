33654b8971638431e1f3b702622beed2199e4054
Matheus Kamer <matheus_kamer@hotmail.com>
2025-03-04 14:55:23 -0400

    feat: add Button component and integrate it into Add screen; update styles and layout

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -5,8 +5,9 @@ import { router } from "expo-router";
 import { styles } from "./styles";
 import { colors } from "@/styles/colors";
 
-import { Categories } from "@/components/categories";
 import { Input } from "@/components/input";
+import { Button } from "@/components/button";
+import { Categories } from "@/components/categories";
 
 export default function Add() {
   return (
@@ -26,6 +27,7 @@ export default function Add() {
       <View style={styles.form}>
         <Input placeholder="Nome" />
         <Input placeholder="URL" />
+        <Button title="Adicionar" />
       </View>
     </View>
   );
